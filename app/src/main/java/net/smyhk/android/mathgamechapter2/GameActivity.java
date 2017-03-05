983b1e4f5b10049ce032efd4e390b6511a1aca03
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-05 13:44:21 -0800

    Implement answer choice buttons

--- app/src/main/java/net/smyhk/android/mathgamechapter2/GameActivity.java
+++ app/src/main/java/net/smyhk/android/mathgamechapter2/GameActivity.java
@@ -2,36 +2,96 @@ package net.smyhk.android.mathgamechapter2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
+
+    private int mLeftFactor;
+    private int mRightFactor;
+    private int mCorrectAnswer;
+    private int mWrongAnswer1;
+    private int mWrongAnswer2;
+    private int answerGiven;
+
+    private TextView mLeftFactorTextView;
+    private TextView mRightFactorTextView;
+
+    private Button mButtonChoiceOne;
+    private Button mButtonChoiceTwo;
+    private Button mButtonChoiceThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        int mLeftFactor = 9;
-        int mRightFactor = 9;
-        int mCorrect_answer = mLeftFactor * mRightFactor;
-        int mWrongAnswer1 = mCorrect_answer + 1;
-        int mWrongAnswer2 = mCorrect_answer - 1;
+        mLeftFactor = 9;
+        mRightFactor = 9;
+        mCorrectAnswer = mLeftFactor * mRightFactor;
+        mWrongAnswer1 = mCorrectAnswer + 1;
+        mWrongAnswer2 = mCorrectAnswer - 1;
 
-        TextView mLeftFactorTextView = (TextView) findViewById(R.id.left_factor);
+        mLeftFactorTextView = (TextView) findViewById(R.id.left_factor);
         mLeftFactorTextView.setText("" + mLeftFactor);
 
-        TextView mRightFactorTextView = (TextView) findViewById(R.id.right_factor);
-        mRightFactorTextView.setText(" " + mRightFactor);
+        mRightFactorTextView = (TextView) findViewById(R.id.right_factor);
+        mRightFactorTextView.setText("" + mRightFactor);
 
-        Button mButtonChoiceOne = (Button) findViewById(R.id.button_choice1);
-        mButtonChoiceOne.setText("" + mCorrect_answer);
+        mButtonChoiceOne = (Button) findViewById(R.id.button_choice1);
+        mButtonChoiceOne.setText("" + mCorrectAnswer);
+        mButtonChoiceOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerGiven = Integer.parseInt("" + mButtonChoiceOne.getText());
 
-        Button mButtonChoiceTwo = (Button) findViewById(R.id.button_choice2);
+                // validate answer
+                if (answerGiven == mCorrectAnswer) {
+                    Toast.makeText(getApplicationContext(),
+                            "Well done!", Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry, try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        mButtonChoiceTwo = (Button) findViewById(R.id.button_choice2);
         mButtonChoiceTwo.setText("" + mWrongAnswer1);
+        mButtonChoiceTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerGiven = Integer.parseInt("" + mButtonChoiceTwo.getText());
 
-        Button mButtonChoiceThree = (Button) findViewById(R.id.button_choice3);
+                // validate answer
+                if (answerGiven == mCorrectAnswer) {
+                    Toast.makeText(getApplicationContext(),
+                            "Well done!", Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry, try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        mButtonChoiceThree = (Button) findViewById(R.id.button_choice3);
         mButtonChoiceThree.setText("" + mWrongAnswer2);
+        mButtonChoiceThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answerGiven = Integer.parseInt("" + mButtonChoiceTwo.getText());
+
+                // validate answer
+                if (answerGiven == mCorrectAnswer) {
+                    Toast.makeText(getApplicationContext(),
+                            "Well done!", Toast.LENGTH_LONG).show();
+                } else{
+                    Toast.makeText(getApplicationContext(),
+                            "Sorry, try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
