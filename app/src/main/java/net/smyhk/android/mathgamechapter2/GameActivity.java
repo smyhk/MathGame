93de6f567a4f35274dc2082f8fb2c2abd35798d7
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-04 19:59:52 -0800

    define TextView and Button n GameActivity

--- app/src/main/java/net/smyhk/android/mathgamechapter2/GameActivity.java
+++ app/src/main/java/net/smyhk/android/mathgamechapter2/GameActivity.java
@@ -2,6 +2,9 @@ package net.smyhk.android.mathgamechapter2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        int leftFactor = 0;
+        int rightFactor = 0;
+        int correct_answer = 0;
+        int wrongAnswer1 = 0;
+        int worngAnswer2 = 0;
+
+        TextView mLeftFactor = (TextView) findViewById(R.id.left_factor);
+
+        TextView mRightFactor = (TextView) findViewById(R.id.right_factor);
+
+        Button mChoiceOne = (Button) findViewById(R.id.button_choice1);
+
+        Button mChoiceTwo = (Button) findViewById(R.id.button_choice2);
+
+        Button mChoiceThree = (Button) findViewById(R.id.button_choice3);
     }
 }
